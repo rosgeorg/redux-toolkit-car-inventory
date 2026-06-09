ce93e27fd2e216df8f829bad8971d6656cb89e57
Jorge Salas <js.2701e@gmail.com>
2026-06-08 19:51:14 -0600

    chore: configure Redux store

--- src/index.js
+++ src/index.js
@@ -4,8 +4,15 @@ import React from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App";
 
+import { Provider } from "react-redux";
+import { store } from "./app/store";
+
 const el = document.querySelector("#root");
 const root = createRoot(el);
 
 /* Implement Redux store here */
-root.render(<App />);
+root.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+);
